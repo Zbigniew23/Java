078dbc1ea3f07747ac62e36237ad1d3be0d4af16
Zbigniew23 <36309387+Zbigniew23@users.noreply.github.com>
2018-02-12 23:28:54 +0100

    git status

--- Palindrom3.java
+++ Palindrom3.java
@@ -8,10 +8,10 @@ public class Palindrom3
 	
 	public static void main(String[] args)
 	{
-		Palindrom3 pal = new Palindrom3();
 		 System.out.println(Palindrom3.isPalindrome(x));
 		 System.out.println(Palindrom3.isPalindrome(y));
 		 System.out.println(Palindrom3.isPalindrome(z));
+		 System.out.println(Palindrom3.isPalindrome(78952));
 	}
 	
 	private static boolean isPalindrome(Integer u) {

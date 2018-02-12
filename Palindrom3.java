64b98192e95ee8193a98858f540267912a79f3a7
Zbigniew23 <36309387+Zbigniew23@users.noreply.github.com>
2018-02-12 19:40:46 +0100

    Add files via upload

---
+++ Palindrom3.java
@@ -0,0 +1,27 @@
+package com.interview.questions;
+
+public class Palindrom3
+{
+  static int x = 11122111;
+  static int y = 115556811;
+  static int z = 171;
+	
+	public static void main(String[] args)
+	{
+		Palindrom3 pal = new Palindrom3();
+		 System.out.println(Palindrom3.isPalindrome(x));
+		 System.out.println(Palindrom3.isPalindrome(y));
+		 System.out.println(Palindrom3.isPalindrome(z));
+		 System.out.println(Palindrom3.isPalindrome(11479));
+	}
+	
+	private static boolean isPalindrome(Integer u) {
+        if (u == 0)
+            return false;
+        String ir =  Integer.toString(u);
+//      StringBuilder isn't thread-safe
+        StringBuilder sb = new StringBuilder(ir);
+       sb.reverse();     
+       return sb.toString().equals(ir);
+    }
+}
